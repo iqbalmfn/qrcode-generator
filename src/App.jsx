54f071ea097f0f929e930238a86c5d17c94e8750
Iqbal Muhammad Fajar Nuralam <iqbalmfn@gmail.com>
2025-01-14 09:53:03 +0700

    feat: tambah fitur upload logo

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,7 @@ function App() {
 
   return (
     <div>
-      <h1>QR Code Generator</h1>
+      <h1 style={{marginBottom:'-15px'}}>QR Code Generator</h1>
       <p>
         by <a href="https://iqbalmfn.com">iqbalmfn</a>
       </p>
